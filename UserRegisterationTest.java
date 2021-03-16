9591583e4c0a86c3afa89b68d07cd707756a2495
Deep Saha <deepsaha288@yahoo.com>
2021-03-16 20:28:23 +0530

    All simple email check validation

--- UserRegisterationTest.java
+++ UserRegisterationTest.java
@@ -73,4 +73,34 @@ public class UserRegisterationTest {
         Assertions.assertTrue(result);
 
     }
+      @Test
+
+    public void All_Email_valid_True() {
+
+        allEmails = new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+                "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+                "abc+100@gmail.com"};
+
+            for (String emailIDs: allEmails) {
+            result = user.checkEmail.validate(emailIDs);
+            System.out.println(result);
+            Assertions.assertTrue(result);
+
+        }
+    }
+    @Test
+    public void All_Email_validation_false() {
+
+        allEmails = new String[]{"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com",
+                "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com",
+                "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+
+                    for (String emailIDs: allEmails) {
+                    result = user.checkEmail.validate(emailIDs);
+                    System.out.println(result);
+                    Assertions.assertFalse(result);
+
+
+        }
+    }
 }
